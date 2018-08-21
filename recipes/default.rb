d7405ba9fccf7ef101e8cae76310076301cb3665
Nick Rycar <rycar@chef.io>
2018-08-21 13:18:39 -0700

    First Commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, Nick Rycar, All Rights Reserved.
+
+package 'apache2'
+
+service 'apache2' do
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do # ~FC033
+  source 'index.html.erb'
+end
